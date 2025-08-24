6deafe1c784c60227590bdfb719eecde329aeb7f
Nalin-Kumar2004 <nalinnow@gmail.com>
2025-08-24 11:04:41 +0530

    initialize frontend with Vite, React, and Tailwind CSS and making folder structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-gray-100 min-h-screen">
+      <h1 className="text-3xl font-bold text-center text-blue-600 p-4">
+        BIT Mesra Lost & Found
+      </h1>
+    </div>
+  );
+}
+
+export default App;
